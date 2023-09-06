bac0894be4ee64478a3cb2adb71683fbe2d641bf
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-06 19:40:10 +0700

    fix(ex-1 ,2 ,3): send an assignment form gai

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,11 @@
 // Exercise #1: Event Conditions
 
 // Start coding here
-let isOver18 = 19;
-let hasCriminalBlacklist;
-let isThai;
+//James
+let isOver18 = true;
+let hasCriminalBlacklist = false;
+let isThai = false;
 
-let isAllow;
+let isAllow = isOver18 && !hasCriminalBlacklist && isThai ;
 
 console.log(isAllow);
